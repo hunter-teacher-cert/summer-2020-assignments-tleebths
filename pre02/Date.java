a22bf0b1e3b1298cf0f9a90df7421d5d3933124e
Tsee Lee <tlee@bths.edu>
2020-06-22 09:45:58 -0400

    finished Think Java 2.1.2

---
+++ pre02/Date.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Date {
+	public static void main(String[] args) {
+		String day;
+		int date, month, year;
+		
+		day = "Monday";
+		date = 22;
+		month = 6;
+		year = 2020;
+/*
+		System.out.println(day);
+		System.out.println(date);
+		System.out.println(month);
+		System.out.println(year);
+*/		
+		System.out.println("Today is " + day + ", " + month + "/" + date + "/" + year + ".");
+	}
+}

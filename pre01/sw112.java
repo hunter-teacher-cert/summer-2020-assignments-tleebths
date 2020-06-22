4a52578b8a30903facef25d91d61f850cc39cf5e
Tsee Lee <tlee@bths.edu>
2020-06-21 23:59:13 -0400

    solved sedgewick 1.1.2

---
+++ pre01/sw112.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class sw112 {
+	public static void main(String[] args) {
+		System.out.println("**********  **");
+		System.out.println("    **      **");
+		System.out.println("    **      **");
+		System.out.println("    **      **");
+		System.out.println("    **      **");
+		System.out.println("    **      **");
+		System.out.println("    **      **********");
+	}
+}

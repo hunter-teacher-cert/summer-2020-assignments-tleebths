7db37b00868b13508932963df5005d1e38217ced
Tsee Lee <tlee@bths.edu>
2020-06-23 21:21:02 -0400

    starting proj

---
+++ pre08/Craps.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Hello {
+	/*
+	 * Returns a random # b/t 1 & n, incl.
+	 */
+	public static int roll(int n) {
+		return n;
+	}
+	
+	/*
+	 * Expects the # of dice, and max value for those dice
+	 * Returns: result of rolling those dice
+	 */
+	public static int shoot(int numDice, int maxValue) {
+		return numDice;
+	}
+	
+	/*
+	 * Returns: Whether the shooter of the round wins or loses
+	 */
+	public static boolean round() {
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}

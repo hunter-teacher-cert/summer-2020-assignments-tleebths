7519b788c7257a22dee572bd1fb2b180b57d87b2
Tsee Lee <tlee@bths.edu>
2020-06-30 21:17:49 -0400

    gonna check for comments

--- day2day/02/GameOfLife.java
+++ day2day/02/GameOfLife.java
@@ -4,14 +4,44 @@ public class GameOfLife {
 
 	public static void main(String[] args) {
 		// set up game board
-		int rows = 10, columns = 10;
-		boolean[][] board = new boolean[rows][columns];  // alive or dead
+		boolean[][] board = seed0();  // alive or dead
 		
+		printBoard(board);
 		System.out.println("After each term, you can press [Enter] to continue, or [a-z] to quit.");
 
 		playGame(board);
 	}  // end main
+	
+	public static boolean[][] seed0() {
+		int rows = 10, columns = 10;
+		boolean[][] board = new boolean[rows][columns];
+		
+		for (int r=0; r<rows; r++) {
+			for (int c=0; c<columns; c++) {
+				board[r][c] = Math.random() < 0.5? true: false;
+			}
+		}  // end outer for
+	
+		return board;
+	}  // end seed0();
+	
+	public static boolean[][] seed2() {
+		boolean[][] board = new boolean[][] {
+			{false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, true, false, false, false, false, false, false},
+			{false, false, false, false, true, false, false, false, false, false},
+			{false, false, true, true, true, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false, false}
+		};
 
+		return board;
+	}  // end seed()
+		
 	// repeats ticks until user wants to stop
 	public static void playGame(boolean[][] board) {
 		Scanner scan = new Scanner(System.in);
@@ -33,6 +63,7 @@ public class GameOfLife {
 
 	// Returns new board after one tick (one update)
 	public static boolean[][] tick(boolean[][] board) {
+		//System.out.println("entering tick");
 		// Set up a second board to not mess up current state, same dims
 		boolean[][] newBoard = new boolean[board.length][board[0].length];
 		
@@ -52,18 +83,22 @@ public class GameOfLife {
      * All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 	*/
 	private static boolean check(boolean[][] board, int r, int c) {
+		//System.out.println("entering check");
 		int counter = 0;  // how many alive
 		
 		// add up the 9 cells incl. board[r][c], then subtract that cell
-		for(int row = r-1; row < row+2; row++) {
-			for(int col = c-1; col < col+2; col++) {
-				counter += board[row][col]? 1: 0;
+		for(int row = r-1; row < r+2; row++) {
+			for(int col = c-1; col < c+2 && row > -1 && row < board.length; col++) {
+				// make sure c is in bounds too
+				if (col > -1 && col < board[row].length)
+					counter += board[row][col]? 1: 0;
 			}
 		}
-		counter -= board[r][c];
+		counter -= board[r][c]? 1: 0;  // overcounted the cell we're examining
 		
-		if 
-		return true;
+		//System.out.println("counter = " + counter);
+		return (board[r][c] && (counter == 2 || counter == 3)) || (!board[r][c] && counter == 3);
+			
 	}
 
 	// prints x for alive, o for dead
@@ -71,9 +106,9 @@ public class GameOfLife {
 		for (int r=0; r < board.length; r++) {
 			for (int c=0; c < board[r].length; c++) {
 				if (board[r][c])
-					System.out.print("x ");
+					System.out.print(Color.GREEN_BOLD_BRIGHT + "X " + Color.RESET);
 				else
-					System.out.print("o ");
+					System.out.print("O ");
 				
 			}  // end for c
 			
@@ -81,4 +116,39 @@ public class GameOfLife {
 		}  // end for r
 	}
 	
+	public static boolean[][] seed1() {
+		boolean[][] board = new boolean[][] {
+			{false, false, false, false, false},
+			{false, false, true, false, false},
+			{false, false, true, false, false},
+			{false, false, true, false, false},
+			{false, false, false, false, false}
+		};
+
+		return board;
+	}  // end seed()
+
+	enum Color {
+		RESET("\033[0m"),
+		RED_BOLD("\033[1;31m"),     // RED
+		GREEN_BOLD("\033[1;32m"),   // GREEN
+		// High Intensity
+		RED_BRIGHT("\033[0;91m"),       // RED
+		GREEN_BRIGHT("\033[0;92m"),     // GREEN
+		// Bold High Intensity
+		RED_BOLD_BRIGHT("\033[1;91m"),      // RED
+		GREEN_BOLD_BRIGHT("\033[1;92m");    // GREEN
+
+		private final String code;
+
+		Color(String code) {
+			this.code = code;
+		}
+
+		@Override
+		public String toString() {
+			return code;
+		}
+	}  // end enum Color
+
 }  // end class

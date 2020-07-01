a9bf28f6df51673f9b66ce7d32d5d2343ac9796c
Tsee Lee <tlee@bths.edu>
2020-07-01 18:32:51 -0400

    finally

--- day2day/02/Life.java
+++ day2day/02/Life.java
@@ -41,8 +41,8 @@ class Life{
 	}  // end seed0();
 
 	// simple infinite loop pattern 1, wiki page
-	public static boolean[][] seed1() {
-		boolean[][] board = new boolean[][] {
+	public static char[][] seed1() {
+		char[][] board = new char[][] {
 			{' ', ' ', ' ', ' ', ' '},
 			{' ', ' ', 'X', ' ', ' '},
 			{' ', ' ', 'X', ' ', ' '},
@@ -54,8 +54,8 @@ class Life{
 	}  // end seed1()
 
 	// spaceship 1, wiki page
-	public static boolean[][] seed2() {
-		boolean[][] board = new boolean[][] {
+	public static char[][] seed2() {
+		char[][] board = new char[][] {
 			{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 			{' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' '},
 			{' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' '},
@@ -82,17 +82,15 @@ class Life{
 //		System.out.println("\n\n------------------\n\n");
 	}
 
-	static void animateBoard(char[][] board) {
-		System.out.print("[0;0H\n");
-		for (int row = 0; row < board.length ; row++) {
-			for (int col = 0; col < board[row].length; col++) {
-				System.out.print(board[row][col]);
-			}
-			System.out.println();
-		}
-	    delay(1000);
-	}  // end animateBoard()
-
+	//helper method to slow down animation 
+	public static void delay(int n)
+	{
+		try {
+			Thread.sleep(n);
+		} 
+		catch(InterruptedException e) {}
+	}
+	
 	/*
 	set the cell (r,c) to value
 	*/
@@ -111,39 +109,76 @@ class Life{
 	to board[r+1][c+1]
 	*/
 	public static int countNeighbours(char[][] board, int r, int c){
-		return 0;
+		int counter = 0;  // how many alive
+
+		// add up the up to 9 cells incl. target, then subtract that cell
+		for(int row = r-1; row < r+2; row++) {
+			for(int col = c-1; col < c+2 && row > -1 && row < board.length; col++) {
+				// make sure c is in bounds too
+				if (col > -1 && col < board[row].length)
+					counter += board[row][col]=='X'? 1: 0;  // ternary operator (if-then-else)
+			}
+		}
+		
+		counter -= board[r][c]=='X'? 1: 0;  // overcounted the cell we're examining
+		//System.out.printf("r=%d c=%d counter=%d", r, c, counter);
+		//System.out.println();
+		return counter;
 	}
 
 	/*
 	given a board and a cell, determine, based on the rules for
 	Conway's GOL if the cell is alive ('X') or dead (' ') in the
 	next generation.
-
 	*/
 	public static char getNextGenCell(char[][] board,int r, int c){
 		// calculate the number of living neighbors around board[r][c]
+		int alive = countNeighbours(board, r, c);
 
 		// determine if board[r][c] is living or dead
 		//    if living and 2 3 neighbors then remain alive
 		//    if dead and 3 neighbors then become alive
-
-		return ' ';
+		return (board[r][c]=='X' && (alive==2 || alive==3)) || ((board[r][c]==' ') && alive==3) ? 'X': ' ';
+		
 	}
 	/*
 	scan the board to generate a NEW board with the
 	next generation
 	*/
-	public char[][] generateNextBoard(char[][] board){
-		char newBoard[][] = new char[25][25];
+	public static char[][] generateNextBoard(char[][] board){
+		int rows = board.length, cols = board[0].length;
+		char newBoard[][] = new char[rows][cols];
+		
 		// fill the new board
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				setCell(newBoard, r, c, getNextGenCell(board, r, c));
+			}
+		}
+		
 		return newBoard;
 	}
 
+	static void animateBoard(char[][] board) {
+		System.out.print("[0;0H\n");
+		for (int row = 0; row < board.length ; row++) {
+			for (int col = 0; col < board[row].length; col++) {
+				System.out.print(board[row][col]);
+			}			
+			System.out.println();
+		}
+	    delay(500);
+	}  // end animateBoard()
+
 	public static void main(String[] args) {
 		char[][] board;
-		board = createNewBoard(25,25);
-		printBoard(board)
-		;
+		board = seed0();
+		//printBoard(board);
+		
+		for (int i=0; i>-1; i++) {
+			animateBoard(board);
+			board = generateNextBoard(board);
+		}
 
 	}
 }

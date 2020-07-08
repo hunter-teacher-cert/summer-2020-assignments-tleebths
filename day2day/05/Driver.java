670b0cc16794a71ce17a0b2abb462b0f199daa94
Tsee Lee <tlee@bths.edu>
2020-07-07 20:21:36 -0400

    finished insert()

--- day2day/05/Driver.java
+++ day2day/05/Driver.java
@@ -15,14 +15,18 @@ public class Driver{
         l.add("Larry");
         // System.out.println(l);
 
-        l.add("Harpo");
-        l.add("Groucho");
-        l.add("Chico");
-        l.add("Zeppo");
-        l.add("Gummo");
+        l.set(0, "Tsee");
+        System.out.println(l.set(1, "tofr"));
+        l.set(3, "Z");
+        l.set(100,"Ddub");
         System.out.println(l);
 
-        
+        l.insert(0, "Margie");
+        l.insert(1, "Jessica");
+        l.insert(6, "Lisa");
+        l.insert(100, "Ddub");
+        System.out.println(l);
+
         /** testing get()
         System.out.printf("Should be Gummo: %s\n", l.get(0));
         System.out.printf("Should be Chico: %s\n", l.get(2));
--- day2day/05/LList.java
+++ day2day/05/LList.java
@@ -39,30 +39,80 @@ public class LList {
     // Set the Node at index to contain value.
     // Returns value currently at index
     // If index is invalid, do nothing.
-    public void set(int index, String value) {
-        // Special case if head==null && index==0.
-        // make new node, set head to new node.
+    public String set(int index, String value) {
+        Node tmp;
+        String oldData = null;
+
+        // Special case if index is 0. If head is null
+        // make new Node(value, head). Otherwise,
+        // Set head to new node. .
+        if (index==0) {
+            if (head == null) {
+                tmp = new Node(value, head);
+                head = tmp;
+            } else {  // Node already exists
+                oldData = head.getData();
+                head.setData(value);
+            }
+
+            return oldData;
+        }  // end special case
+
         // Strategy: Traverse LList up to index - 1.
         // If tmp == null, index is invalid. Else,
         // If tmp.next is NOT null, set its data to value.
         // Else, make new node and point it to what comes
         // AFTER tmp.next, then point tmp to new node.
-        if (head==null && index==0) {
-            head = new Node(value);
-            return;
-        }
-
-        Node tmp = head;
-        for(int i=0; i<index && tmp!=null; i++) {
+        tmp = head;
+        for(int i=0; i < index-1 && tmp!=null; i++) {
             tmp = tmp.getNext();
-        }
+        }  // Draw detailed diagram to understand stopping conditions
 
         if (tmp == null)
             return null;
 
-        return tmp.getData();
+        if (tmp.getNext() == null) {  // basically adding at end of list
+            Node newNode = new Node(value);
+            tmp.setNext(newNode);
+            return null;
+        }
 
-    }
+        oldData = tmp.getNext().getData();
+        tmp.getNext().setData(value);
+        return oldData;
+
+    }  // end set()
+
+    // Insert a new Node containing value at index
+    // If index is invalid, do nothing.
+    public void insert(int index, String value) {
+        Node tmp;
+
+        // Special case if index==0.
+        // make new node, set head to new node.
+        if (index==0) {
+            tmp = new Node(value, head);
+            head = tmp;
+            return;
+        }
+
+        // Strategy: Traverse LList up to index - 1.
+        // If tmp == null, index is invalid. Else,
+        // Make new node and point it to tmp.next.
+        // Point tmp.next to new node.
+        tmp = head;
+        for(int i=0; i < index-1 && tmp!=null; i++) {
+            tmp = tmp.getNext();
+        }  // Draw detailed diagram to understand stopping conditions
+
+        if (tmp == null)
+            return;
+
+        Node newNode = new Node(value, tmp.getNext());
+        tmp.setNext(newNode);
+        return;
+
+    }  // end insert()
 
     // Returns true if the list is empty, false otherwise
     public boolean isEmpty() {

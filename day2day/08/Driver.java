ebdca055a8692fdcf5972418e01636b7e6be3af6
Tsee Lee <tlee@bths.edu>
2020-07-10 18:49:52 -0400

    finalized

--- day2day/08/Driver.java
+++ day2day/08/Driver.java
@@ -6,14 +6,14 @@ public class Driver {
     public static void main(String[] args) {
 
         MyStack s = new MyStack();
-        System.out.println("Printing empty stack:\n" + s);
+        System.out.println("Printing empty stack:\n\n" + s);
         System.out.printf("isEmpty() should be true: %b. Top=\n", s, s.isEmpty(), s.top());
 
-        s.push("Dominion of Canada");
+        // s.push("Dominion of Canada");
         s.push("Z");
         s.push("tofr");
         s.push("dDub");
-        System.out.println("Printing stack ZtdD:\n" + s);
+        System.out.println("Printing stack Ztd(D):\n\n" + s);
         System.out.printf("size %d: Top=%s\n", s.size(), s.top());
 
         System.out.println("pop: " + s.pop());

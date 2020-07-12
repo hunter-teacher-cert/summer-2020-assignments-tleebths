1a8c653a6e068bdd7b6bf91c651815abe8f8b56e
Tsee Lee <tlee@bths.edu>
2020-07-11 22:09:42 -0400

    adding tail recursion

--- day2day/00/Fib.java
+++ day2day/00/Fib.java
@@ -1,40 +1,31 @@
 import java.util.*;
 
-public class Fib {
+public class FibTail {
 
-	/* 
-	 * Returns the fib sequence from 0 to i
-	 * Since arrays are passed by reference, we can change values within
-	 * Definitions: fib(0) = 0, fib(1) = 1
+	/*
+	 * Returns an int, fib(a)
+	 * Parameter contains 3 elements [a, f0, f1], such that:
+	 * f0 and f1 start as seeds, the first two numbers of the series.
+	 * They're the accumulators representing fib(n-1) and fib(n-2).
+	 * a is initially the a-th fib # we want. It will be decremented
+	 * at each turn. Initially, f(0) = f0, f(1)=f1.
 	 */
-	public static int[] fibonacci(int[] sequence, int i) {
-		if (i == 0) {
-			sequence[0] = 0;
-		} else if (i == 1) {
-			sequence = fibonacci(sequence, i-1);
-			sequence[1] = 1;
+	public static int fib(int[] data) {
+		if (a < 2) {
+			return data[a];
 		} else {
-			sequence = fibonacci(sequence, i-1);
-			sequence[i] = sequence[i-1] + sequence[i-2];
+			data[0]--;
+			temp = data[1] + data[2];  // calculate next in sequence
+			data[1] = data[2];  // updates, keeping only last 2 #s
+			data[2] = temp;     // in the sequence
+			return go(data);
 		}
-		
-		return sequence;
-	}
-	
-	public static int fib(int n) {
-		// create the array to track each fib
-		// To find fib(1), we need fib(0) and fib(1)
-		int[] sequence = new int[n+1];
-		
-		//process the rest of the fibs from fib(n-1)
-		sequence = fibonacci(sequence, n);
-		return sequence[n];
-		
-	}
-	
+
+	}  // end go()
+
     public static void main(String[] args) {
-    	int n = Integer.parseInt( args[0] );  // arg from command-line
-		
+    	int f0 = Integer.parseInt( args[0] );  // arg from command-line
+
     	System.out.print("fib(" + n + ") = ");
     	System.out.println(fib(n));
 

c1b2138c5a90a99e8d72030f252380c7e1591b99
Tsee Lee <tlee@bths.edu>
2020-07-13 00:26:59 -0400

    commented out debugging code, made ez version

--- day2day/00/Fib_ez.java
+++ day2day/00/Fib_ez.java
@@ -11,25 +11,25 @@ public class Fib {
 	 * a is initially the a-th fib # we want. It will be decremented
 	 * at each turn. Initially, f(0) = f0, f(1)=f1.
 	 */
-	public static int fib(int[] data) {
+	public static int fib(int n, int f0, int f1) {
 		// System.out.print("Entering fib, data=" + Arrays.toString(data));
-		int a = data[0];
+		// int a = data[0];
 		// System.out.print(" a=" + a);
 
-		if (a < 2) {
-			return data[a+1];
+		if (n < 2) {
+			return f0;
 		} else {
 			// modifies input array to avoid making new one
-			data[0]--;
-			// System.out.print(" f0=" + data[1]);
-			// System.out.print(" f1=" + data[2]);
+			n--;
+			// System.out.print(" f0=" + f0);
+			// System.out.print(" f1=" + f1);
 
-			int temp = data[1] + data[2];  // calculate next in sequence
-			data[1] = data[2];  // updates, keeping only last 2 #s
-			data[2] = temp;     // in the sequence
+			int next = f0 + f1;  // calculate next in sequence
+			f0 = f1;  // updates, keeping only last 2 #s
+			f1 = next;     // in the sequence
 
-			// System.out.println("exiting " + data[0]);
-			return fib(data);
+			// System.out.println("exiting " + n);
+			return fib(n, f0, f1);
 		}
 
 	}  // end go()
@@ -42,9 +42,8 @@ public class Fib {
     	int n = Integer.parseInt( args[0] );
 		int f0 = Integer.parseInt( args[1] );
 		int f1 = Integer.parseInt( args[2] );
-		int[] data = new int[]{n, f0, f1};
 
-    	System.out.printf("\nfib(%d) = %d", n, fib(data));
+    	System.out.printf("\nfib(%d) = %d", n, fib(n, f0, f1));
 
 	}
 }

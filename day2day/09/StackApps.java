cfa13ba6aad1cd6567be5f39684337f2d0cdb16f
Tsee Lee <tlee@bths.edu>
2020-07-14 06:49:15 -0400

    it's all abstracted

--- day2day/09/StackApps.java
+++ day2day/09/StackApps.java
@@ -6,36 +6,43 @@ public class StackApps {
 	// Returns s in reverse order by word
 	// Assume s is trimmed of spaces, and only one space b/t words
 	public static String revByWord(String s) {
-		// Cut sentence into stack of Strings
-		Stack<String> ws = str2words(s);
+		return stk2str(str2words(s), " ");  // Cut sentence into stack of Strings
+	}  // end revByWord()
 
-		String rev = ws.pop();
-		while (!ws.empty()) {
-			rev += " " + ws.pop();
+	// Returns a stack of words as Strings, given a sentence s
+	// Assumes s is trimmed of spaces & is not null
+	private static Stack<String> str2words(String s) {
+		Stack<String> ws = new Stack<String>();  // words, to form sentence
+		String word = "";
+
+		// Process each letter of String into a word.
+		// At each space, push word onto stack. Reset word.
+		for (int i=0; i < s.length(); i++) {
+			char c = s.charAt(i);
+
+			if (c != ' ') {  // end of a word
+				word += c;
+			} else {
+				ws.push(word);
+				word = "";
+			}
 		}
 
-		return rev;
-	}  // end revByWord()
+		ws.push(word);  // last word doesn't end with a space
+		return ws;
+	}  // end str2stk()
 
 	public static boolean isPalindromeByWord(String s) {
 		// Convert String to Stack of words, s1
 		// Reverse s by word, then convert to Stack of words, s2
-		// Pop both and see if anything's different
-		Stack<String> ss1 = str2words(s);
-		Stack<String> ss2 = str2words(revByWord(s));
-
-		while (!ss1.empty()) {
-			if (!ss1.pop().equals( ss2.pop() ))
-				return false;
-		}
-
-		return true;
+		// Check if they're the same
+		return stk2str(str2words(s), " ").equals( str2words(revByWord(s)) );
 	}  // end isPalindromeByWord()
 
 
 	// Returns s in reverse order
 	public static String reverse(String s) {
-		return letters2str(str2letters(s));  // push into stack, pop back out
+		return stk2str(str2letters(s), "");  // push into stack, pop back out
 	}
 
 	// Returns true or false depneding on whether s is a palindrome
@@ -45,19 +52,19 @@ public class StackApps {
 	}
 
 	// Returns whether two letters are opposite brackets
-	public static boolean opps(char s1, char s2) {
+	private static boolean opps(char s1, char s2) {
 		return (s1=='(' && s2==')')
 			|| (s1=='[' && s2==']')
 			|| (s1=='{' && s2=='}');
 	}
 
 	// Returns whether c is an opening bracket
-	public static boolean isOpening(char c) {
+	private static boolean isOpening(char c) {
 		return c=='(' || c=='[' || c=='{';
 	}
 
 	// Returns whether c is a closing bracket
-	public static boolean isClosing(char c) {
+	private static boolean isClosing(char c) {
 		return c==')' || c==']' || c=='}';
 	}
 
@@ -100,50 +107,34 @@ public class StackApps {
 		return bs.empty();
 	}  // end parenCheck()
 
-	// Returns a String from a given Stack of letters
-	private static String letters2str(Stack<String> ls) {
-		String s = "";
+	// Returns a String from a given Stack of String
+	// separated by the delimiter sep (usu. "" or " ").
+	private static String stk2str(Stack<String> ls, String sep) {
+		String s;
+
+		if (ls.empty()) {
+			s = "";
+		} else {
+			s = ls.pop();
+		}
+
 		while (!ls.empty()) {
-			s += ls.pop();
+			s += sep + ls.pop();
 		}
 
 		return s;
-	}
+	}  // end stk2str()
 
 	// Returns a stack of letters as Strings, given a sentence s
 	// assumes s is not null
 	private static Stack<String> str2letters(String s) {
 		Stack<String> ls = new Stack<String>();  // letters stack
-		String str = "";  // to cast char to String by concatenating
 
 		for (int i=0; i < s.length(); i++) {
-			ls.push( str + s.charAt(i) );
+			ls.push( "" + s.charAt(i) );  // to cast char to String
 		}
 
 		return ls;
 	}
-
-	// Returns a stack of words as Strings, given a sentence s
-	// assumes s is not null
-	private static Stack<String> str2words(String s) {
-		Stack<String> ws = new Stack<String>();  // words, to form sentence
-		String word = "";
-
-		// Process each letter of String into a word.
-		// At each space, push word onto stack. Reset word.
-		for (int i=0; i < s.length(); i++) {
-			char c = s.charAt(i);
-
-			if (c != ' ') {  // end of a word
-				word += c;
-			} else {
-				ws.push(word);
-				word = "";
-			}
-		}
-
-		ws.push(word);  // last word doesn't end with a space
-		return ws;
-	}  // end str2stk()
 
 }  // end class

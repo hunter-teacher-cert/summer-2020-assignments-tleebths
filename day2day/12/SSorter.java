b874ed61b0fb8b58ee932dcae8e0011dae8ac442
Tsee Lee <tlee@bths.edu>
2020-07-16 14:01:52 -0400

    simplified

--- day2day/12/SSorter.java
+++ day2day/12/SSorter.java
@@ -7,6 +7,34 @@ import java.util.*;
 public class SSorter
 {
 
+    //##################################################
+    //##################################################
+
+    // run with command line argument specifying length of array list
+    public static void main( String[] args )
+    {
+        int len = 5;
+        if (args.length>0)
+            len = Integer.parseInt(args[0]);
+
+        ArrayList<Integer> randos = prestoArrayListo(len, 0, 100);
+
+        System.out.println("unsorted : " + randos);
+
+        // long start = System.currentTimeMillis();
+        ArrayList<Integer> sorted = mergeSort(randos);
+        System.out.println("merge    : " + sorted);
+
+        // randos = prestoArrayListo(10, 0, 100);
+        selectionSort(randos);
+        System.out.println("selection: " + randos);
+
+        // long elapsed = System.currentTimeMillis() - start;
+        // System.out.println("Elapsed time: " + elapsed);
+
+
+    }  // end main()
+
 
     //return ArrayList of random ints on range [lo,lo+hi)
     public static ArrayList<Integer> prestoArrayListo(int numItems, int lo, int hi)
@@ -60,28 +88,38 @@ public class SSorter
     }
 
     public static ArrayList<Integer> mergeSort(ArrayList<Integer> al) {
-        return mergeSort(al, 0, al.size() - 1);
+        return mergeSort(al, 0, al.size());
     }
 
     // helper for mergeSort()
+    // from index lo up to but not incl. hi
     private static ArrayList<Integer> mergeSort(ArrayList<Integer> al, int lo, int hi) {
-        if (al.size() < 2)
-            return al;
-
-        int mid = (lo+hi)/2;
-
-        // dividing phase: sort each half
-        mergeSort(al, lo, mid);
-        mergeSort(al, mid+1, hi);
-
-        // conquering phase: merge in order
-        int i1 = lo, i2 = mid+1;  // indices to each sub-list
-        int ele1, ele2;  // to hold actual values
         ArrayList<Integer> sorted = new ArrayList<Integer>();
 
-        while (i1<=mid && i2<=hi;) {
-            ele1 = (int)al.get(i1);
-            ele2 = (int)al.get(i2);
+        // base cases
+        if (lo>hi)
+            return sorted;
+
+        if (lo == hi-1) {
+            sorted.add(al.get(lo));
+            return sorted;
+        }
+
+        // System.out.printf("ms: lo=%d, hi=%d\n", lo, hi);
+
+        // dividing phase: sort each half
+        int mid = (lo+hi)/2;
+        ArrayList<Integer> al1 = mergeSort(al, lo, mid);
+        ArrayList<Integer> al2 = mergeSort(al, mid, hi);
+        int sz1 = al1.size(), sz2 = al2.size();
+
+        // conquering phase: merge in order
+        int i1 = 0, i2 = 0;  // indices to each sub-list
+        int ele1, ele2;  // to hold actual values
+
+        while (i1<sz1 && i2<sz2) {
+            ele1 = (int)al1.get(i1);
+            ele2 = (int)al2.get(i2);
 
             if (ele1 < ele2) {
                 sorted.add(ele1);
@@ -93,40 +131,20 @@ public class SSorter
 
         }  // end while--at least one of the sub-lists will be "empty"
 
-        while (i1 <= mid) {
-            ele1 = (int)al.get(i1);
+        // copy non-empty list into the return array
+        while (i1<sz1) {
+            ele1 = (int)al1.get(i1);
             sorted.add(ele1);
             i1++;
         }
 
-        while (i2 <= hi) {
-            sorted.add((int)al.get(i2++));
+        while (i2<sz2) {
+            sorted.add((int)al2.get(i2++));
         }
 
         return sorted;
 
-    }//selectionSort
+    }  // mergeSort helper
 
-
-
-    //##################################################
-    //##################################################
-
-    public static void main( String[] args )
-    {
-        ArrayList<Integer> randos = prestoArrayListo(100000, 0, 100);
-
-        // System.out.println(randos);
-
-        long start = System.currentTimeMillis();
-
-        selectionSort(randos);
-
-        long elapsed = System.currentTimeMillis() - start;
-
-        // System.out.println(randos);
-        System.out.println("Elapsed time: " + elapsed);
-
-    }//end main
 
 }//end class

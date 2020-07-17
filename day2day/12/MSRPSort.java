a9028288fea9ae3271e2fa70d11c68b8b49b36f7
Tsee Lee <tlee@bths.edu>
2020-07-17 19:09:20 -0400

    starting new work

--- day2day/12/MSRPSort.java
+++ day2day/12/MSRPSort.java
@@ -1,10 +1,13 @@
-/**
+/** Sorting algorithms, incl.
+ *  Merge sort, Selection sort, Recursive selection sort,
+ *  double selection sort, Pivot (quick) sort
+ *  by Tsee Lee, July 2020
 **/
 
 import java.io.*;
 import java.util.*;
 
-public class SSorter
+public class MSRPSort
 {
 
     //##################################################
@@ -13,7 +16,7 @@ public class SSorter
     // run with command line argument specifying length of array list
     public static void main( String[] args )
     {
-        int len = 5;
+        int len = 1;
         if (args.length>0)
             len = Integer.parseInt(args[0]);
 
@@ -22,18 +25,54 @@ public class SSorter
         System.out.println("unsorted : " + randos);
 
         // long start = System.currentTimeMillis();
-        ArrayList<Integer> sorted = mergeSort(randos);
+        ArrayList<Integer> sorted = quickSort(randos);
+        System.out.println("merge    : " + sorted);
+
+        // long start = System.currentTimeMillis();
+        sorted = mergeSort(randos);
         System.out.println("merge    : " + sorted);
 
         // randos = prestoArrayListo(10, 0, 100);
-        selectionSort(randos);
-        System.out.println("selection: " + randos);
+        // selectionSort(randos);
+        // System.out.println("selection: " + randos);
 
         // long elapsed = System.currentTimeMillis() - start;
         // System.out.println("Elapsed time: " + elapsed);
 
 
     }  // end main()
+
+    // aka. pivot sort
+    public static ArrayList<Integer> quickSort(ArrayList<Integer> al) {
+        if (al==null || al.size()==0) {
+            return al;
+        }
+
+        return quickSort(al, 0, al.size());
+    }
+
+    // Helper method, sorts from index lo up to but not incl. hi
+    // Assuming >= 1 item in list
+    private static ArrayList<Integer> quickSort(ArrayList<Integer> al, int lo, int hi) {
+        ArrayList<Integer> sorted = new ArrayList<Integer>();
+
+        // Base case: if 1 item or less in list, it's sorted
+        if (lo == hi) {
+            return sorted;
+        } else if (lo == hi - 1) {
+            sorted.add(al.get(lo));
+        }
+
+        /* Recursive case: Picks a pivot, then creates 3 new lists
+         * List 1: all elements smaller than pivot
+         * List 2: all elements equal to pivot
+         * List 3: all elements greater than pivot
+         * Sort all 3 lists, then concatenates them
+         */
+
+
+        return null;
+    }  // end quickSort helper
 
 
     //return ArrayList of random ints on range [lo,lo+hi)
@@ -57,27 +96,62 @@ public class SSorter
 
     // find and return the index of the smallest value in the
     // ArrayList al from index lo to index hi inclusive
-    public static int findSmallest(ArrayList<Integer> al, int lo, int hi){
-        int maxIndexSoFar = lo;
-        for (int i = lo; i <=hi; i++) {
-            if (al.get(i) < al.get(maxIndexSoFar)){
-                maxIndexSoFar = i;
+    public static int findSmallest(ArrayList<Integer> al, int lo, int hi) {
+        // inefficient to check so much -- solutions?
+        if (al==null || lo<0 || lo>hi || hi>=al.size())
+        return -1;
+
+        // assume the first item is the smallest - call it our smallest so far
+        int minIndex = lo;
+        Integer minValue = al.get(lo);
+        Integer item;
+
+        // loop over all the items
+        for (int i = lo+1; i <= hi; i++) {
+            item = al.get(i);
+
+            //   if the current item is smaller than the smalleest so far,
+            //      then the current item becomes the new smallest so far
+            if (item.compareTo(minValue) < 0) {
+                minValue = item;
+                minIndex = i;
             }
-        }
-        return maxIndexSoFar;
+
+        }  // end for loop
+
+        // change this to return the correct answer
+        return minIndex;
     }
 
-    public static void selectionSort(ArrayList<Integer> al) {
+    // Returns [x,y] such that x is the index of the min of al from lo to hi
+    // and y is the index of the local max
+    private static int[] findLocalMinMax(ArrayList<Integer> al, int lo, int hi) {
 
-        int lo = 0;
-        int temp;
+        // assume the first item is the smallest - call it our smallest so far
+        int minIndex = lo, maxIndex = lo;
+        Integer minValue = al.get(lo), maxValue = minValue;
 
-        for (int i=0; i < al.size() - 1; i++) {
-            lo = findSmallest(al, i, al.size()-1);
-            swap(al, i, lo);
-        }
+        Integer item;
 
-    }//selectionSort
+        // loop over all the items
+        for (int i = lo+1; i <= hi; i++) {
+            item = al.get(i);
+
+            //   if the current item is smaller than the smalleest so far,
+            //      then the current item becomes the new smallest so far
+            if (item.compareTo(minValue) < 0) {
+                minValue = item;
+                minIndex = i;
+            } else if (item.compareTo(maxValue) > 0) {
+                maxValue = item;
+                maxIndex = i;
+            }
+
+        }  // end for loop
+
+        // change this to return the correct answer
+        return new int[]{minIndex, maxIndex};
+    }
 
     /* Swaps elements at the two given indices
     */
@@ -86,6 +160,85 @@ public class SSorter
         al.set(i1, al.get(i2));
         al.set(i2, temp);
     }
+
+    /* Returns the ArrayList in sorted form using selection sort
+    * Strategy: Calls helper function on whole list
+    */
+    public static void selectionSort(ArrayList al) {
+        if (al==null)
+        return;
+
+        selectionSort(al, 0, al.size()-1);
+    }
+
+    /* Recursively performs selection sort on given range of given list
+    * Strategy: Find index of minValue to the end of the list
+    * Swap values at current and minValue indices if different
+    * Sort the rest of the list
+    */
+    public static void selectionSort(ArrayList al, int lo, int hi) {
+        // required even if this method's turned private
+        if (al==null || al.size() < 2 || lo<0 || lo>hi || hi>=al.size())
+            return;
+
+        int i = lo;  // index of "first" element
+        int j = findSmallest(al, lo, hi);  // index of min.
+
+        if (i != j) {
+            swap(al, i, j);
+        }
+
+        selectionSort(al, lo+1, hi);
+
+    }  // end selectionSort helper
+
+    /* Recursively performs selection sort on given range of given list
+    * Strategy: Iterate through all indices from 0 to hi-1
+    * Find index of minValue to the end of the list
+    * Swap values at current and minValue indices
+    */
+    public static void selectionSortIter(ArrayList al) {
+        if (al==null || al.size() < 2)
+        return;
+
+        int hi = al.size() - 1;
+
+        for(int i=0; i<=hi; i++) {
+            int min = findSmallest(al, i, hi);
+
+            if (i != min)
+            swap(al, i, min);
+        }  // end for loop
+
+    }  // end selectionSortHelper()
+
+    /* Returns al in sorted order by selection sorting in both directions
+     * On each pass, find index of both min & max in unsorted range
+     * swap them to left & right ends, respectively
+     */
+    public static void dblSelectionSort(ArrayList al) {
+        if (al==null || al.size() < 2)
+            return;
+
+        int hi = al.size() - 1;
+
+        for(int lo=0; lo<=hi; lo++, hi--) {
+            int[] minMax = findLocalMinMax(al, lo, hi);
+            // System.out.println(al + " " + minMax[0] + " " + minMax[1]);
+
+            if (lo != minMax[0])
+                swap(al, lo, minMax[0]);  // what if max was at lo?
+
+            if (hi != minMax[1]) {
+                if (lo != minMax[1]) {
+                    swap(al, hi, minMax[1]);
+                } else {
+                    swap(al, hi, minMax[0]);  // would've been moved
+                }
+            }  // end nested if
+        }  // end for loop
+
+    }  // end selectionSortHelper()
 
     public static ArrayList<Integer> mergeSort(ArrayList<Integer> al) {
         return mergeSort(al, 0, al.size());
